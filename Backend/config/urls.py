feba831ec3fd5cc71d048d1eec184b740c7c0d6c
nathan <mouyabinathan@gmail.com>
2026-07-10 16:34:57 +0100

    superuser

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -6,6 +6,8 @@ from terrain.api import router as terrain_router
 from reservations.api import router as reservations_router
 from facturation.api import router as facturation_router
 from terrain.reporting import router as reporting_router
+from django.http import HttpResponse
+import subprocess
 
 # ---------- API ----------
 api = NinjaAPI(title="Gestion Cimetière API", version="1.0.0")
@@ -15,8 +17,22 @@ api.add_router("/reservations", reservations_router)
 api.add_router("/facturation", facturation_router)
 api.add_router("/reporting", reporting_router)
 
+# ---------- Création superuser ----------
+def create_superuser(request):
+    try:
+        result = subprocess.run(
+            ['python', 'Backend/create_superuser.py'],
+            capture_output=True,
+            text=True,
+            cwd='.'  # Ajoute ce paramètre pour être sûr du chemin
+        )
+        return HttpResponse(result.stdout + result.stderr, content_type="text/plain")
+    except Exception as e:
+        return HttpResponse(f"Erreur: {e}", status=500)
+
 # ---------- URLs ----------
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
+    path('create-superuser/', create_superuser),
 ]
