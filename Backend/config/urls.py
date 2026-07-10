a76becf5d39851be8570df0b6b2db2d10d551336
nathan <mouyabinathan@gmail.com>
2026-07-10 17:02:35 +0100

    superuser

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -6,9 +6,6 @@ from terrain.api import router as terrain_router
 from reservations.api import router as reservations_router
 from facturation.api import router as facturation_router
 from terrain.reporting import router as reporting_router
-from django.http import HttpResponse
-import subprocess
-import os
 
 # ---------- API ----------
 api = NinjaAPI(title="Gestion Cimetière API", version="1.0.0")
@@ -18,23 +15,23 @@ api.add_router("/reservations", reservations_router)
 api.add_router("/facturation", facturation_router)
 api.add_router("/reporting", reporting_router)
 
-# ---------- Création superuser ----------
-def create_superuser(request):
-    try:
-        # On exécute le script dans le dossier Backend
-        script_path = os.path.join(os.path.dirname(__file__), 'create_superuser.py')
-        result = subprocess.run(
-            ['python', script_path],
-            capture_output=True,
-            text=True
-        )
-        return HttpResponse(result.stdout + result.stderr, content_type="text/plain")
-    except Exception as e:
-        return HttpResponse(f"Erreur: {e}", status=500)
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+def make_superuser(request):
+    user = User.objects.get(email="mouyabinatan@gmail.com")
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+    return HttpResponse("Superuser créé avec succès.")
+
+
 
 # ---------- URLs ----------
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
-    path('create-superuser/', create_superuser),
+    path('make-superuser/', make_superuser),
 ]
+
